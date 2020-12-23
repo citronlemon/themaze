fc9e96f9f7de34680c41b23cdcce5fbebdb36ef7
citronlemon <76525051+citronlemon@users.noreply.github.com>
2020-12-24 00:33:40 +0900

    ver 2
    -타이틀 변경
    -모달창 추가

---
+++ base.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".modal_btn").click(function(){
+
+        $(".modal").show(50);
+    })
+
+
+    $(".modal_content").click(function(){
+        $(".modal").hide(50);
+    })
+
+});
